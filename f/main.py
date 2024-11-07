c8faf715b86cbbaf279944210b81f1158def78f0
kimgyuvin <143357046+kimgyuvin@users.noreply.github.com>
2024-11-07 17:55:02 +0700

    Add files via upload

---
+++ f/main.py
@@ -0,0 +1,46 @@
+import requests
+import random
+import time
+import os
+from colorama import Fore
+
+
+time.sleep(1)
+
+channel_id = input("Masukkan ID channel: ")
+waktu = int(input("Set Waktu Kirim Pesan: "))
+
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+with open("pesan.txt", "r") as f:
+    words = f.readlines()
+
+with open("token.txt", "r") as f:
+    authorization = f.readline().strip()
+
+while True:
+        channel_id = channel_id.strip()
+
+        payload = {
+            'content': random.choice(words).strip()
+        }
+
+        headers = {
+            'Authorization': authorization
+        }
+
+        r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+        print(Fore.WHITE + "Sent message: ")
+        print(Fore.YELLOW + payload['content'])
+
+        response = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+
+        time.sleep(waktu)
